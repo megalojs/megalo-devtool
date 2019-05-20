9185c2d38f3d67a9bb889db849158521256650a6
elcarim5efil <elcarim5efil@gmail.com>
2019-05-20 18:08:44 +0800

    fix: remove page cache in server

--- server/handlers/dev.js
+++ server/handlers/dev.js
@@ -1,31 +1,5 @@
-const pageManager = require('./page-manager');
-
-const components = {
-  launch(ctx, { data }) {
-    pageManager.clear();
-    if (data.versions) {
-      pageManager.syncVersions(data.versions);
-    }
-  },
-  mounted(ctx, { data }) {
-    pageManager.addPage(data.pageInfo.id, data);
-  },
-  updated(ctx, { data }) {
-    pageManager.syncComponent(data.pageInfo.id, data.component);
-  },
-  beforeDestroy(ctx, { type, data }) {
-    if (type === 'page') {
-      pageManager.removePage(data.pageInfo.id, data);
-    }
-  },
-};
-
 module.exports = {
   message(ctx, req) {
     ctx.io.namespace.ui.emit('broadcast', req);
-
-    if (req.module === 'components' && components[req.lifecycle]) {
-      components[req.lifecycle](ctx, req);
-    }
   },
 };
--- server/handlers/ui.js
+++ server/handlers/ui.js
@@ -1,5 +1,4 @@
 const log = require('../utils/log');
-const pageManager = require('./page-manager');
 
 async function manualRefresh(ctx, data, fn) {
   const { sockets } = ctx.io.namespace.dev;
@@ -12,17 +11,6 @@ async function manualRefresh(ctx, data, fn) {
   if (socket) {
     socket.emit('refreshPages', ({ pages, versions, stores }) => {
       log('refreshPages', pages);
-
-      pageManager.clear();
-      pages.forEach((page) => {
-        pageManager.addPage(page.pageInfo.id, page);
-      });
-
-
-      if (versions.vue) {
-        pageManager.syncVersions(versions);
-      }
-
       fn({ versions, pages, stores });
     });
   }
